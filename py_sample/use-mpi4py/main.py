4ec325d6409728e9bbe60790bd8ec738df3b9d36
wararaki <ky7.ott.w@gmail.com>
2025-07-31 23:20:36 +0900

    update

--- py_sample/use-mpi4py/main.py
+++ py_sample/use-mpi4py/main.py
@@ -1,16 +1,20 @@
 from mpi4py import MPI
 
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
+
+def main() -> None:
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+
+    if rank == 0:
+        data = {'a': 7, 'b': 3.14}
+        comm.send(data, dest=1, tag=11)
+        print("send")
+    elif rank == 1:
+        data = comm.recv(source=0, tag=11)
+        print("recieve: ", data)
+
+    print("DONE")
 
 
-if rank == 0:
-    data = {'a': 7, 'b': 3.14}
-    comm.send(data, dest=1, tag=11)
-    print("send")
-elif rank == 1:
-    data = comm.recv(source=0, tag=11)
-    print("recieve: ", data)
-
-
-print("DONE")
+if __name__ == "__main__":
+    main()
